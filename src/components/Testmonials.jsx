d558bab35dded8718c0d33cc707eabb729b87d4f
Muhammad Hassan Jawwad <113373773+hassanjawwad12@users.noreply.github.com>
2024-12-20 13:25:43 +0500

    Update Testmonials.jsx

--- src/components/Testmonials.jsx
+++ src/components/Testmonials.jsx
@@ -8,9 +8,9 @@ import star from '../assets/star.png'
 
 const Testmonials = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-white text-black w-full h-[79vh] py-2 gap-4">
+    <div className="flex flex-col items-center justify-center bg-white text-black w-full md:h-[79vh] h-[82vh] py-2 gap-4">
       <p className="text-3xl font-extrabold">Our Clients</p>
-      <p className="w-[50%] text-lg italic text-center">
+      <p className="w-[50%] md:text-lg text-sm italic text-center">
         Our rich history with our client go back to 2021. We started collecting
         data in a traditional way but later on as our portfolio became diverse
         new and improved data collection was implemented. From sources such as
@@ -21,13 +21,13 @@ const Testmonials = () => {
         Commerce. We strictly follow the privacy and compliance guidelines and
         protect the data
       </p>
-      <div className="flex flex-row justify-between w-full items-center px-10 mt-6">
-        <img src={Bramerz} alt="Bramerz" className="h-28 w-auto" />
-        <img src={star} alt="Starcom" className="h-28 w-auto" />
-        <img src={nestle} alt="Nestle" className="h-28 w-auto" />
-        <img src={Cheetay} alt="Cheetay" className="h-16 w-auto" />
-        <img src={Nrtc} alt="NRTC" className="h-36 w-auto" />
-        <img src={commerce} alt="Commerce" className="h-28 w-auto" />
+      <div className="flex flex-wrap  justify-between w-full items-center px-10 mt-6">
+        <img src={Bramerz} alt="Bramerz" className="md:h-28 h-16 w-auto" />
+        <img src={star} alt="Starcom" className="md:h-28 h-16 w-auto" />
+        <img src={nestle} alt="Nestle" className="md:h-28 h-16 w-auto" />
+        <img src={Cheetay} alt="Cheetay" className="md:h-16 h-10 w-auto" />
+        <img src={Nrtc} alt="NRTC" className="md:h-36 h-20 w-auto" />
+        <img src={commerce} alt="Commerce" className="md:h-28 h-16 w-auto" />
       </div>
     </div>
   );
